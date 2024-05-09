60f69241fbdb16339fa94d6f323fc576d5fd1c89
Huy Phan <huy.phan2210@outlook.com>
2024-05-09 08:54:50 +0700

    Add images + sass package

--- reactapp1.client/src/api/job-api.ts
+++ reactapp1.client/src/api/job-api.ts
@@ -1,21 +1,21 @@
 import axios, { AxiosInstance } from "axios";
-import { WeatherForecastService, serviceOptions } from "../swagger/api";
+import { JobsListingService, serviceOptions } from "../swagger/api";
 
 export default class JobAPI {
   axiosInstance: AxiosInstance = axios.create({
     baseURL: "https://localhost:5173",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
   });
 
   constructor() {
     serviceOptions.axios = this.axiosInstance;
   }
 
-  async populateWeatherData() {
+  async getJobs() {
     // Now you should be able to call the static method getWeatherForecast
-    return await WeatherForecastService.getWeatherForecast();
+    return await JobsListingService.getJobs();
   }
 }
